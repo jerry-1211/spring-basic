1cda3a39299f367f3f5bcc1a0f87ad00b5fb163e
jerry-1211 <kcjerim1234567@gmail.com>
2025-03-31 23:24:24 +0900

    spring 컨테이너 도입

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -2,27 +2,37 @@ package Hello.core;
 
 import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.discount.RateDiscountPolicy;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
 import Hello.core.member.MemoryMemberRepository;
 import Hello.core.order.OrderService;
 import Hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
