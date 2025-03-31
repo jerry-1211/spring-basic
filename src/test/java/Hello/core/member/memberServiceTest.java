024a20b797dcb16b86e6ff9d04dff6bff4bad6e1
Jerry <mac@macuiMacBookPro.localdomain>
2025-03-31 18:09:17 +0900

    간단 프로젝트 시작

---
+++ src/test/java/Hello/core/member/memberServiceTest.java
@@ -0,0 +1,24 @@
+package Hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+public class memberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memberA",Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+}
