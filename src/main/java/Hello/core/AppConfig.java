4d33b03fe610405dcf434b0370713f41669c351e
jerry-1211 <kcjerim1234567@gmail.com>
2025-03-31 22:31:50 +0900

    AppConfig 리팩터링 - 중복 제거 - 역할에 따른 구현 - 애플리케이션 전체 구성을 한번에 파악 가능

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package Hello.core;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
@@ -9,11 +10,19 @@ import Hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
